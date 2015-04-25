276a02e5d2d84bc4ba2df361556345ac3fcd8bbd
dfreire <dario.freire@gmail.com>
2015-04-25 14:05:41 +0100

    Small fixes

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "1. In the container run: service apache2 start"
-echo "2. In the host, access http://$(boot2docker ip):3000/"
+echo "1. In the container, run: service apache2 start"
+echo "2. In the host, access: http://$(boot2docker ip):3000/"
 echo ""
 
 eval "$(boot2docker shellinit)"
