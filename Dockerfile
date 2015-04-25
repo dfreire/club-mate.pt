04af56e018339efb90e3267c5270b7015c806ba8
dfreire <dario.freire@gmail.com>
2015-04-25 14:03:01 +0100

    Created and activated apache config for club-mate.pt

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,13 @@ FROM linode/lamp
 
 MAINTAINER dfreire <dario.freire@gmail.com>
 
-WORKDIR /var/www/example.com/public_html/
+RUN mkdir /var/www/club-mate.pt
+RUN mkdir /var/www/club-mate.pt/public_html
+RUN mkdir /var/www/club-mate.pt/log
+RUN mkdir /var/www/club-mate.pt/backups
+
+ADD club-mate.pt.conf /etc/apache2/sites-available/
+
+RUN a2ensite club-mate.pt.conf
+
+WORKDIR /var/www/club-mate.pt/public_html/
--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "1. In the container run: service apache2 start"
+echo "2. In the host, access http://$(boot2docker ip):3000/"
+echo ""
+
 eval "$(boot2docker shellinit)"
-boot2docker ip
-docker run -i -t --rm -p 3000:80 -v `pwd`/public_html:/var/www/example.com/public_html/ club-mate /bin/bash && cd /club-mate
+docker run -i -t --rm -p 3000:80 -v `pwd`/public_html:/var/www/club-mate.pt/public_html/ club-mate /bin/bash && cd /club-mate
