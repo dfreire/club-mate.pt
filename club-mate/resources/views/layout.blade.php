5725ecf4513159dfe0e73c676cc347993ec98f1a
dfreire <dario.freire@gmail.com>
2015-04-28 22:52:40 +0100

    update

--- club-mate/resources/views/layout.blade.php
+++ club-mate/resources/views/layout.blade.php
@@ -20,19 +20,52 @@
 </head>
 <body>
 
-	<p>
-		<a href="/">Club Mate</a> |
-		<a href="/sobre-o-club-mate">Sobre o Club Mate</a> |
-		<a href="/yerba-mate">Yerba Mate</a> |
-		<a href="/cocktails">Cocktails</a> |
-		<a href="/onde-encontrar">Onde Encontrar</a> |
-		<a href="/contactos">Contactos</a>
-	</p>
+	<div id="wrapper">
 
-	@yield('content')
+        <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+					<a href="/">Club Mate</a>
+                </li>
+                <li><a href="/sobre-o-club-mate">Sobre o Club Mate</a></li>
+                <li><a href="/yerba-mate">Yerba Mate</a></li>
+                <li><a href="/cocktails">Cocktails</a></li>
+                <li><a href="/onde-encontrar">Onde Encontrar</a></li>
+                <li><a href="/contactos">Contactos</a></li>
+            </ul>
+        </div>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+						@yield('content')
+                        <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /#page-content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+
 
 	<!-- Scripts -->
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+	<!-- Menu Toggle Script -->
+	<script>
+	$("#menu-toggle").click(function(e) {
+		e.preventDefault();
+		$("#wrapper").toggleClass("toggled");
+	});
+	</script>
+
 </body>
 </html>
