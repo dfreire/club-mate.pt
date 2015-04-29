e711da76472978d57376e30d36fa7b81e61683e8
dfreire <dario.freire@gmail.com>
2015-04-29 22:38:39 +0100

    update

--- club-mate/resources/views/layout.blade.php
+++ club-mate/resources/views/layout.blade.php
@@ -22,7 +22,6 @@
 
 	<div id="wrapper">
 
-        <!-- Sidebar -->
         <div id="sidebar-wrapper">
             <ul class="sidebar-nav">
                 <li class="sidebar-brand">
@@ -34,24 +33,24 @@
                 <li><a href="/onde-encontrar">Onde Encontrar</a></li>
                 <li><a href="/contactos">Contactos</a></li>
             </ul>
-        </div>
-        <!-- /#sidebar-wrapper -->
+        </div><!-- /#sidebar-wrapper -->
 
-        <!-- Page Content -->
         <div id="page-content-wrapper">
             <div class="container-fluid">
+				<div class="row">
+					<div class="col-lg-12">
+						<a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
+					</div>
+				</div>
                 <div class="row">
                     <div class="col-lg-12">
 						@yield('content')
-                        <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
                     </div>
                 </div>
             </div>
-        </div>
-        <!-- /#page-content-wrapper -->
+        </div><!-- /#page-content-wrapper -->
 
-    </div>
-    <!-- /#wrapper -->
+    </div><!-- /#wrapper -->
 
 
 
