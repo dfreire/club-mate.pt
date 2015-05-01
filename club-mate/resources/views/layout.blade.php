702b000853108082fd23c5c9f51897cf650510ec
dfreire <dario.freire@gmail.com>
2015-05-01 16:30:48 +0100

    update

--- club-mate/resources/views/layout.blade.php
+++ club-mate/resources/views/layout.blade.php
@@ -11,8 +11,6 @@
 	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
 
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
@@ -25,25 +23,28 @@
 	<div id="wrapper">
 
         <div id="sidebar-wrapper">
-            <ul class="sidebar-nav">
-                <li class="sidebar-brand">
-					<a href="/">Club Mate</a>
-                </li>
-                <li><a href="/sobre-o-club-mate">Sobre o Club Mate</a></li>
-                <li><a href="/yerba-mate">Yerba Mate</a></li>
-                <li><a href="/cocktails">Cocktails</a></li>
-                <li><a href="/onde-encontrar">Onde Encontrar</a></li>
-                <li><a href="/contactos">Contactos</a></li>
-            </ul>
+			<div>
+	            <ul class="sidebar-nav">
+	                <li class="sidebar-brand">
+						<a href="/">Club Mate</a>
+	                </li>
+	                <li><a href="/sobre-o-club-mate">Sobre o Club Mate</a></li>
+	                <li><a href="/yerba-mate">Yerba Mate</a></li>
+	                <li><a href="/cocktails">Cocktails</a></li>
+	                <li><a href="/onde-encontrar">Onde Encontrar</a></li>
+	                <li><a href="/contactos">Contactos</a></li>
+	            </ul>
+				<!--
+					<a href="mailto:info@club-mate.pt">info@club-mate.pt</a>
+				-->
+			</div>
         </div><!-- /#sidebar-wrapper -->
 
         <div id="page-content-wrapper">
             <div class="container-fluid">
 				<div class="row">
 					<div class="col-lg-12">
-						<h3 id="menu-toggle">
-							<span class="glyphicon glyphicon-menu-hamburger"></span>
-						</h3>
+						<a id="menu-toggle" class="glyphicon" href="#"></a>
 					</div>
 				</div>
                 <div class="row">
@@ -64,10 +65,20 @@
 
 	<!-- Menu Toggle Script -->
 	<script>
+	function toggle() {
+		var isToggled = $("#wrapper").hasClass("toggled");
+		console.warn("isToggled: " + isToggled);
+		$("#menu-toggle").toggleClass("glyphicon-remove", !isToggled);
+		console.warn("glyphicon-remove: " + $("#menu-toggle").hasClass("glyphicon-remove"));
+		$("#menu-toggle").toggleClass("glyphicon-menu-hamburger", isToggled);
+		console.warn("glyphicon-ok: " + $("#menu-toggle").hasClass("glyphicon-menu-hamburger"));
+	}
 	$("#menu-toggle").click(function(e) {
 		e.preventDefault();
 		$("#wrapper").toggleClass("toggled");
+		toggle();
 	});
+	$( toggle );
 	</script>
 
 </body>
