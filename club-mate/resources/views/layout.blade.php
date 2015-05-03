ccf38bc06aa1b0adcca526cc57f0cc7a7e55b8ef
dfreire <dario.freire@gmail.com>
2015-05-03 22:12:51 +0100

    update

--- club-mate/resources/views/layout.blade.php
+++ club-mate/resources/views/layout.blade.php
@@ -92,5 +92,7 @@
 	$(onResize);
 	</script>
 
+	@yield('scripts')
+
 </body>
 </html>
--- club-mate/resources/views/pages/onde_encontrar.blade.php
+++ club-mate/resources/views/pages/onde_encontrar.blade.php
@@ -1,5 +1,22 @@
 @extends('layout')
 
 @section('content')
-<h1>Onde Encontrar</h1>
+<div class="row">
+    <div class="col-lg-12">
+        <h1>Onde Encontrar</h1>
+    </div>
+</div>
+@stop
+
+@section('scripts')
+<script type="text/javascript">
+function initialize() {
+    var mapOptions = {
+        center: { lat: -34.397, lng: 150.644},
+        zoom: 8
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+</script>
 @stop
