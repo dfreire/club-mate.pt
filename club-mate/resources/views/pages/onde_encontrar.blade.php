a43b33fc5182ce14874439960ab878eb2f8b86f8
dfreire <dario.freire@gmail.com>
2015-05-05 22:23:59 +0100

    update

--- club-mate/resources/views/pages/onde_encontrar.blade.php
+++ club-mate/resources/views/pages/onde_encontrar.blade.php
@@ -5,10 +5,20 @@
 <script type="text/javascript">
 function initialize() {
     var mapOptions = {
-        center: { lat: -34.397, lng: 150.644},
-        zoom: 8
+        center: new google.maps.LatLng(39.6833333,-8.1166667),
+        zoom: 6
     };
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(41.148056, -8.642722),
+        map: map,
+        title: "Restaurante Casa D'Oro"
+    });
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(38.7159764,-9.1467839),
+      map: map,
+      title: "Pizza à Pezzi"
+  });
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 </script>
@@ -21,8 +31,22 @@ google.maps.event.addDomListener(window, 'load', initialize);
     </div>
 </div>
 <div class="row">
-    <div class="col-lg-12">
+    <div class="col-lg-8">
         <div id="map-canvas"></div>
+    </div>
+    <div class="col-lg-4">
+        <address>
+          <strong>Restaurante Casa D'Oro</strong><br/>
+          Rua do Ouro, Nº797<br/>
+          4150 Porto<br/>
+          <a href="https://www.google.pt/maps/@41.148056,-8.642722,20z">41.148056, -8.642722</a><br/>
+        </address>
+        <address>
+          <strong>Restaurante Pizza à Pezzi</strong><br/>
+          Rua Dom Pedro V, Nº84 (Príncipe Real)<br/>
+          1250-001 Lisboa<br/>
+          <a href="https://www.google.pt/maps/@38.7159764,-9.1467839,20z">38.7159764, -9.1467839</a><br/>
+        </address>
     </div>
 </div>
 @stop
@@ -32,8 +56,9 @@ google.maps.event.addDomListener(window, 'load', initialize);
 $(window).resize(function () {
     var h = $(window).height(),
         offsetTop = 150; // Calculate the top offset
+        marginBotton = 30;
 
-    $('#map-canvas').css('height', (h - offsetTop));
+    $('#map-canvas').css('height', (h - offsetTop - marginBotton)).css('margin-bottom', marginBotton);
 }).resize();
 </script>
 @stop
