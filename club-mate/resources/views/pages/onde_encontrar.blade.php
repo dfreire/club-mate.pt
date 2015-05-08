dd7e8947a1673e892fa425b1a4f98e38825430d4
dfreire <dario.freire@gmail.com>
2015-05-08 17:41:34 +0100

    update

--- club-mate/resources/views/pages/onde_encontrar.blade.php
+++ club-mate/resources/views/pages/onde_encontrar.blade.php
@@ -45,18 +45,22 @@ google.maps.event.addDomListener(window, 'load', initialize);
         <div id="map-canvas"></div>
     </div>
     <div class="col-lg-4">
-        <address>
-          <strong>Restaurante Casa D'Oro</strong><br/>
-          Rua do Ouro, Nº797<br/>
-          4150 Porto<br/>
-          <a href="https://www.google.pt/maps/@41.148056,-8.642722,20z">41.148056, -8.642722</a><br/>
-        </address>
-        <address>
-          <strong>Restaurante Pizza à Pezzi</strong><br/>
-          Rua Dom Pedro V, Nº84 (Príncipe Real)<br/>
-          1250-001 Lisboa<br/>
-          <a href="https://www.google.pt/maps/@38.7159764,-9.1467839,20z">38.7159764, -9.1467839</a><br/>
-        </address>
+        <div id="casa-d-oro">
+            <address>
+                <strong>Restaurante Casa D'Oro</strong><br/>
+                Rua do Ouro, Nº797<br/>
+                4150 Porto<br/>
+                <a href="https://www.google.pt/maps/@41.148056,-8.642722,20z">41.148056, -8.642722</a><br/>
+            </address>
+        </div>
+        <div id="pizza-a-pezzi">
+            <address>
+                <strong>Restaurante Pizza à Pezzi</strong><br/>
+                Rua Dom Pedro V, Nº84 (Príncipe Real)<br/>
+                1250-001 Lisboa<br/>
+                <a href="https://www.google.pt/maps/@38.7159764,-9.1467839,20z">38.7159764, -9.1467839</a><br/>
+            </address>
+        </div>
     </div>
 </div>
 @stop
