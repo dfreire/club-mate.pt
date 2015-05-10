9f693d3f21364d0cc13510397bbc0a548af14a93
dfreire <dario.freire@gmail.com>
2015-05-10 21:10:41 +0100

    update

--- club-mate/resources/views/pages/sobre_o_club_mate.blade.php
+++ club-mate/resources/views/pages/sobre_o_club_mate.blade.php
@@ -1,5 +1,12 @@
 @extends('layout')
 
 @section('content')
-<h1>Sobre o Club-Mate</h1>
+<div class="row">
+    <div class="col-lg-12">
+        <h1>Sobre o Club-Mate</h1>
+        <br/>
+    </div>
+</div>
+<div class="row">
+</div>
 @stop
