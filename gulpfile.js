b845bf48cb36e3be3394f4376dc7a3cb5281d32b
dfreire <dario.freire@gmail.com>
2015-05-16 15:19:11 +0100

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,42 @@
 var gulp = require('gulp'),
-	babel = require('gulp-babel'),
-	sass = require('gulp-sass'),
-	cached = require('gulp-cached'),
-	watch = require('gulp-watch');
+    babel = require('gulp-babel'),
+    sass = require('gulp-sass'),
+    cached = require('gulp-cached'),
+    watch = require('gulp-watch');
 
-gulp.task('html', function() {
-	return gulp.src('client/**/*.html')
-		.pipe(gulp.dest('public'));
+gulp.task('static', function() {
+    gulp.src('client/**/*.html')
+        .pipe(gulp.dest('public'));
+    gulp.src('client/**/*.ico')
+        .pipe(gulp.dest('public'));
+    gulp.src('client/**/*.png')
+        .pipe(gulp.dest('public'));
+    gulp.src('client/**/*.jpg')
+        .pipe(gulp.dest('public'));
 });
 
 gulp.task('scss', function() {
-	return gulp.src('client/scss/main.scss')
-		.pipe(sass())
-		.pipe(gulp.dest('public/css'));
+    gulp.src('client/scss/main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('public/css'));
 });
 
 gulp.task('jsx', function() {
-	return gulp.src('client/jsx/**/*.jsx')
-		.pipe(cached('scripts'))
-		.pipe(babel({
-			modules: 'amd'
-		}))
-		.pipe(gulp.dest('public/js'));
+    gulp.src('client/jsx/**/*.jsx')
+        .pipe(cached('scripts'))
+        .pipe(babel({
+            modules: 'amd'
+        }))
+        .pipe(gulp.dest('public/js'));
 });
 
-gulp.task('watch', ['html', 'scss', 'jsx'], function() {
-	gulp.watch("client/**/*.html", ['html']);
-	gulp.watch("client/**/*.jsx", ['jsx']);
-	gulp.watch("client/**/*.scss", ['scss']);
+gulp.task('watch', ['static', 'scss', 'jsx'], function() {
+    gulp.watch("client/**/*.html", ['static']);
+    gulp.watch("client/**/*.ico", ['static']);
+    gulp.watch("client/**/*.png", ['static']);
+    gulp.watch("client/**/*.jpg", ['static']);
+    gulp.watch("client/**/*.jsx", ['jsx']);
+    gulp.watch("client/**/*.scss", ['scss']);
 });
 
-gulp.task('default', ['html', 'scss', 'jsx'], function() {});
+gulp.task('default', ['static', 'scss', 'jsx'], function() {});
