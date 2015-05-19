21cf2d213c05beb066fbc5c8dba114ded71bea29
dfreire <dario.freire@gmail.com>
2015-05-19 22:47:58 +0100

    update

--- client/jsx/main.jsx
+++ client/jsx/main.jsx
@@ -10,14 +10,43 @@ import {Contacts} from "contacts";
 var Container = React.createClass({
     render: function() {
         return (
-            <div>
-                <Link to="/sobre-o-club-mate">Sobre o Club-Mate</Link>&nbsp;|&nbsp;
-                <Link to="/yerba-mate">Yerba Mate</Link>&nbsp;|&nbsp;
-                <Link to="/cocktails">Cocktails</Link>&nbsp;|&nbsp;
-                <Link to="/onde-encontrar">Onde Encontrar</Link>&nbsp;|&nbsp;
-                <Link to="/contactos">Contactos</Link>
-                <div className="container">
-                    <RouteHandler/>
+            <div id="wrapper">
+                <div id="sidebar-wrapper">
+        			<div>
+        	            <ul className="sidebar-nav">
+        	                <li className="sidebar-brand"><img src="/img/club-mate-logo.png" /></li>
+    	                    <li><Link to="/sobre-o-club-mate">Sobre o Club-Mate</Link></li>
+        	                <li><Link to="/yerba-mate">Yerba Mate</Link></li>
+        	                <li><Link to="/cocktails">Cocktails</Link></li>
+        	                <li><Link to="/onde-encontrar">Onde Encontrar</Link></li>
+        	                <li><Link to="/contactos">Contactos</Link></li>
+        	            </ul>
+        			</div>
+        			<div className="my-contact-wrapper">
+        				<a className="my-contact" href="mailto:info@club-mate.pt">info@club-mate.pt</a>
+        			</div>
+        			<div>
+        				<ul className="my-social-contacts-wrapper">
+        					<li><a href="#"><i className="fa fa-facebook-square"></i></a></li>
+        					<li><a href="#"><i className="fa fa-twitter-square"></i></a></li>
+        					<li><a href="#"><i className="fa fa-linkedin-square"></i></a></li>
+        					<li><a href="#"><i className="fa fa-pinterest-square"></i></a></li>
+        				</ul>
+        			</div>
+                </div>
+                <div id="page-content-wrapper">
+                    <div className="container-fluid">
+        				<div className="row">
+        					<div className="col-lg-12">
+        						<a href="#" id="menu-toggle" className="fa">&nbsp;</a>
+        					</div>
+        				</div>
+                        <div className="row">
+                            <div className="col-lg-12">
+                                <RouteHandler/>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
