524240ec134a2e8d016ec86fce0b126c3696b119
dfreire <dario.freire@gmail.com>
2015-05-21 21:17:23 +0100

    update

--- react2/client/app/about.jsx
+++ react2/client/app/about.jsx
@@ -1,31 +1,25 @@
 import React from "react/addons";
 import {GoogleMaps} from "react-google-maps";
 
-/*
- * Sample From: https://developers.google.com/maps/documentation/javascript/examples/map-simple
- */
 class SimpleMap extends React.Component {
-  /*
-   * 1. Create a component that wraps all your map sub-components.
-   */
-  render () {
-    const {props, state} = this,
-          {googleMapsApi, otherProps} = props;
-    /*
-     * 2. Render GoogleMaps component with containerProps
-     */
-    return (
-      <GoogleMaps containerProps={{
-          otherProps,
-          style: {
-            height: "100%",
-          },
-        }}
-        googleMapsApi={google.maps}
-        zoom={8}
-        center={{lat: -34.397, lng: 150.644}} />
-    );
-  }
+
+    render () {
+        console.log("render map");
+        const {props, state} = this,
+        {googleMapsApi, otherProps} = props;
+
+        return (
+            <GoogleMaps containerProps={{
+                    otherProps,
+                    style: {
+                        height: "300px",
+                    },
+                }}
+                googleMapsApi={google.maps}
+                zoom={8}
+                center={{lat: -34.397, lng: 150.644}} />
+        );
+    }
 
 }
 
