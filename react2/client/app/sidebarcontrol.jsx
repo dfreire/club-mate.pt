b63305b3cf0caa6d05c78b419f94286744da87f3
dfreire <dario.freire@gmail.com>
2015-05-23 11:36:40 +0100

    update

--- react2/client/app/sidebarcontrol.jsx
+++ react2/client/app/sidebarcontrol.jsx
@@ -1,13 +1,33 @@
 import React from "react/addons";
 import Router from "react-router";
-var Link = Router.Link;
+import classNames from "classnames";
 
 export var SideBarControl = React.createClass({
     mixins: [Router.State],
 
+    getInitialState: function() {
+        return {
+            visible: true
+        }
+    },
+
     render: function() {
+        console.warn("visible: ", this.state.visible);
         return (
-            <a href="#" id="menu-toggle" className="fa">&nbsp;</a>
+            <a className={this.getClassName()} onClick={this.handleClick} href="#" id="menu-toggle">&nbsp;</a>
         );
+    },
+
+    getClassName: function () {
+        if (this.state.visible) {
+            return classNames('fa', 'fa-times');
+        } else {
+            return classNames('fa', 'fa-bars');
+        }
+    },
+
+    handleClick: function(e) {
+        e.preventDefault();
+        this.setState({ visible: !this.state.visible });
     }
 });
