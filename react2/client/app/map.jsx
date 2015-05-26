1a74da23ffa5228f5477c7034e10fd56fe679d76
dfreire <dario.freire@gmail.com>
2015-05-26 22:17:14 +0100

    update

--- react2/client/app/map.jsx
+++ react2/client/app/map.jsx
@@ -1,18 +1,28 @@
 import React from "react/addons";
-import {GoogleMaps, Marker} from "react-google-maps";
+import {GoogleMaps, Marker, InfoWindow} from "react-google-maps";
 
 class Map extends React.Component {
 
     constructor(args) {
         super(args);
-        this.state = { marks: [] };
+        this.state = { zoom: 6, marks: [], markWithInfo: undefined };
         this.state.marks.push({ position: { lat: 41.148056,  lng: -8.642722  }, key: "casa-d-oro" });
         this.state.marks.push({ position: { lat: 38.7159764, lng: -9.1467839 }, key: "pizza-a-pezzi" });
+        //this.state.markWithInfo = this.state.marks[0];
+    }
+
+    _handleMarkerClick(mark) {
+        console.warn("_handleMarkerClick", mark);
+        this.setState({ markWithInfo: mark });
     }
 
     render() {
+        var that = this;
+        var hasMarkWithInfo = _.isObject(this.state.markWithInfo);
+        console.warn("render markWithInfo", hasMarkWithInfo);
         const {props, state} = this,
         {googleMapsApi, otherProps} = props;
+
 
         return (
             <GoogleMaps containerProps={{
@@ -22,10 +32,25 @@ class Map extends React.Component {
                     },
                 }}
                 googleMapsApi={google.maps}
-                zoom={6}
+                zoom={this.state.zoom}
                 center={{lat: 39.6833333, lng: -8.1166667}}>
                 {this.state.marks.map(function(mark, index) {
-                    return <Marker position={mark.position} key={mark.key} icon="/img/club-mate-marker.png" />;
+                    var markerTag = <Marker
+                        position={mark.position}
+                        key={mark.key}
+                        onClick={function() {
+                            that._handleMarkerClick(mark);
+                        }}
+                        icon="/img/club-mate-marker.png" />;
+
+                    if (mark === that.state.markWithInfo) {
+                        return <div>{React.addons.createFragment({
+                                a: { markerTag },
+                                b: <InfoWindow position={that.state.markWithInfo.position} content="hello" />
+                        })}</div>
+                    } else {
+                        return { markerTag };
+                    }
                 })}
             </GoogleMaps>
 
