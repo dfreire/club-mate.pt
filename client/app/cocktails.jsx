33ad3472526b08284ed06288f1b066db23b28d1c
dfreire <dario.freire@gmail.com>
2015-05-30 13:31:36 +0100

    update

--- client/app/cocktails.jsx
+++ client/app/cocktails.jsx
@@ -1,12 +1,77 @@
 import React from "react/addons";
 import Router from "react-router";
 
+var data = [{
+    name: "Mojito-Mate",
+    ingredients: [
+        "Lima",
+        "Açúcar amarelo",
+        "Gelo em cubo ou picado",
+        "Rum branco",
+        "Hortelã",
+        "Club-Mate"
+    ],
+    description: "Esmagar as limas com o açúcar e adicionar gelo. De seguida acrescentar 4 a 6 cl de rum, club-mate a gosto, e finalizar com folhas de hortelã frescas. Misturar e beber!"
+}, {
+    name: "Vodka-Mate",
+    ingredients: [
+        "Vodka",
+        "Limão ou lima",
+        "Gelo em cubo",
+        "Club-Mate"
+    ],
+    description: ""
+}, {
+    name: "Whisky-Mate",
+    ingredients: [
+        "Whisky",
+        "Gelo em cubo",
+        "Club-Mate"
+    ],
+    description: ""
+/*
+}, {
+    name: "",
+    ingredients: [
+        "",
+        "",
+        "",
+        ""
+    ],
+    description: ""
+*/
+}];
+
 export var Cocktails = React.createClass({
     mixins: [Router.State],
 
     render: function() {
         return (
-            <h1>Cocktails</h1>
+            <div>
+                <div className="row">
+                    <div className="col-lg-12">
+                        <h1>Cocktails</h1>
+                    </div>
+                </div>
+                <div className="row">
+                    {data.map(function(cocktail, index) {
+                        var ingredientList = cocktail.ingredients.map(function(ingredient) {
+                            return (<li>{ingredient}</li>);
+                        });
+                        return (
+                            <div className="col-lg-4">
+                                <div style={{background: "#eee", padding: "20px"}}>
+                                    <h3 style={{marginTop: "0px"}}>{cocktail.name}</h3>
+                                    <ul>
+                                        {ingredientList}
+                                    </ul>
+                                    <p style={{textAlign: "justify"}}>{cocktail.description}</p>
+                                </div>
+                            </div>
+                        );
+                    }.bind(this))}
+                </div>
+            </div>
         );
     }
 });
