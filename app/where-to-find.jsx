8dc9040bf76caee678e0a2287bbcc575643b5daf
dfreire <dario.freire@gmail.com>
2015-06-21 20:15:28 +0100

    update

--- app/where-to-find.jsx
+++ app/where-to-find.jsx
@@ -3,7 +3,7 @@ import Router from "react-router";
 
 import Map from "./map.jsx";
 
-var Marks = _.sortBy([{
+var Retaurants = [{
     key: "casa-d-oro",
     name: "Casa D'Oro",
     addressLine1: "Rua do Ouro, 797",
@@ -19,16 +19,6 @@ var Marks = _.sortBy([{
     position: {
         lat: 41.1477907, lng: -8.6149328
     }
-
-}, {
-    key: "maus-habitos",
-    name: "Maus Hábitos",
-    addressLine1: "R. de Passos Manuel, 178, 4º Piso",
-    addressLine2: "4000-382 Porto",
-    position: {
-        lat: 41.146703, lng: -8.605738
-    }
-
 }, {
     key: "black-mamba",
     name: "Black Mamba",
@@ -36,6 +26,28 @@ var Marks = _.sortBy([{
     addressLine2: "4000 Porto",
     position: {
         lat: 41.151276, lng: -8.613917
+    }
+
+/*
+Sins
+
+Rua da Conceição nº 59 Porto
+
+
+Café CCOP
+Rua Duque Loulé nº 202
+Porto
+
+*/    
+}];
+
+var Bars = [{
+    key: "maus-habitos",
+    name: "Maus Hábitos",
+    addressLine1: "R. de Passos Manuel, 178, 4º Piso",
+    addressLine2: "4000-382 Porto",
+    position: {
+        lat: 41.146703, lng: -8.605738
     }
 }, {
     key: "plano-b",
@@ -69,6 +81,42 @@ var Marks = _.sortBy([{
     position: {
         lat: 41.1471739, lng: -8.6143639
     }
+
+    /*
+    candelabro
+
+rua da conceição nº3 Porto
+*/
+
+
+}];
+
+var Groceries = [{
+    /*
+    Petúlia
+
+Rua de Júlio Dinis 775      4150-323 Porto
+
+
+Dona Rosa
+
+Mercado bolhão Loja 1      4000-252 Porto
+
+
+Mercearia Rodela
+
+Praça Guilherme Gomes Fernandes, 66      4050-294 Porto
+
+
+Mercearia Rodela 2
+
+Rua Saraiva Carvalho nº47      4000-520 Porto
+*/
+
+}];
+
+var Marks = _.sortBy([{
+
 /*
 }, {
     key: "",
