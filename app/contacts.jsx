ba62cb71ff329238879b320637da18ad50548707
dfreire <dario.freire@gmail.com>
2015-06-24 18:04:48 +0100

    update

--- app/contacts.jsx
+++ app/contacts.jsx
@@ -9,7 +9,7 @@ export var Contacts = React.createClass({
             <div>
                 <div className="row">
                     <div className="col-lg-12">
-                        <h1>Contactos</h1>
+                        <h1>Contactos / Encomendas</h1>
                     </div>
                 </div>
                 <div className="row">
@@ -19,23 +19,6 @@ export var Contacts = React.createClass({
                             <li style={{marginTop: "15px"}}><strong>E-mail:</strong> <a href="mailto:info@club-mate.pt">info@club-mate.pt</a></li>
                             <li style={{marginTop: "15px"}}><strong>Telefone:</strong> 00351 910277546</li>
                             <li style={{marginTop: "15px"}}><strong>Telefone:</strong> 00351 914926645</li>
-                        </ul>
-                    </div>
-                </div>
-                <br />
-                <div className="row">
-                    <div className="col-lg-12">
-                        <h1>Encomendas</h1>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-lg-6">
-                        <ul style={{listStyle: "none"}}>
-                            <li style={{marginTop: "15px"}}><strong>Caixa club-mate 0,33l (24 garrafas):</strong> 25€</li>
-                            <li style={{marginTop: "15px"}}><strong>Caixa club-mate 0,5 (20 garrafas):</strong> 25€</li>
-                            <li style={{marginTop: "15px"}}><strong>Caixa club-mate cola 0,33l (24 garrafas):</strong> 25€</li>
-                            <li style={{marginTop: "15px"}}>Entregas gratuitas no Porto.</li>
-                            <li style={{marginTop: "15px"}}>Encomenda mínima 2 caixas.</li>
                         </ul>
                     </div>
                 </div>
